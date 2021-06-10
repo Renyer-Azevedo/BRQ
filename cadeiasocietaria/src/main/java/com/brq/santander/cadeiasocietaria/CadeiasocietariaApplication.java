b7c2c63dacd51b3ff3f67a0432f2bc79372b9368
Renyer Almeida de Azevedo <azevedorenyer@brq.com>
2021-06-10 10:22:07 -0300

    cadeia societaria
    criação projeto

---
+++ cadeiasocietaria/src/main/java/com/brq/santander/cadeiasocietaria/CadeiasocietariaApplication.java
@@ -0,0 +1,13 @@
+package com.brq.santander.cadeiasocietaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadeiasocietariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadeiasocietariaApplication.class, args);
+	}
+
+}
---
+++ cadeiasocietaria/src/test/java/com/brq/santander/cadeiasocietaria/CadeiasocietariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brq.santander.cadeiasocietaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadeiasocietariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
